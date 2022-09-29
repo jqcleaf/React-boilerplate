b7a73d585459d1d1b2b796cc1817feb5c021e2ce
cjqxss123 <91357087+cjqxss123@users.noreply.github.com>
2022-09-28 21:26:07 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import "./App.scss";
+
+let a :string = "123"
+console.log(a)
+const App = () => {
+  return (
+    <div className="firstdiv">
+      <div className="seconddiv"></div>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 1234,
+  },
+};
